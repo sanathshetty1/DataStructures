0284ad7b6eac2c2acdeeb7f413c7dbe44da9f128
sourcebits-sanathkumar <sanath.kumar@sourcebits.com>
2014-12-09 09:11:58 +0530

    Third Commit

---
+++ DataStructure/LinkedList/GenricNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    /// <summary>
+    /// Represnts a Node in the linkedlist
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    class GenricNode<T>
+    {
+        public T dataitem;
+        public GenricNode<T> next;
+
+    }
+}
--- DataStructure/LinkedList/MainProgram.cs
+++ DataStructure/LinkedList/MainProgram.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace LinkedList
 {
-    class Program
+    class MainProgram
     {
         static void Main(string[] args)
         {
